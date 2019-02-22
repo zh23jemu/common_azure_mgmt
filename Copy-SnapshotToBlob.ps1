488dbd371b02c31b1cab68d724950b39b8e5f218
zh23jemu <41720280+zh23jemu@users.noreply.github.com>
2019-02-22 11:43:47 +0800

    initial version

---
+++ Copy-SnapshotToBlob.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module -Name AzureRM
+
+Login-AzureRmAccount -EnvironmentName AzureChinaCloud
+
+#Declare Variables
+$resourceGroupName = 'CCNSVBKALL'
+$snapshotName = 'snapshot20190211_mailbkup1'
+$resourceGroupNameStorageAccount = 'CCNSVBKALL'
+$storageAccountName = 'snapshot01'
+$storageContainerName = 'mailbkup1'
+$destinationVHDFileName = 'mailbkup1'
+
+#Get the Storage Account Key of the Destination Storage Account
+$storageAccountKey = Get-AzureRmStorageAccountKey -resourceGroupName $resourceGroupNameStorageAccount -AccountName $storageAccountName
+
+#Generate the SAS for the snapshot
+$sas = Grant-AzureRmSnapshotAccess -ResourceGroupName $resourceGroupName -SnapshotName $snapshotName  -DurationInSecond 3600 -Access Read 
+
+$destinationContext = New-AzureStorageContext –storageAccountName $storageAccountName -StorageAccountKey ($storageAccountKey).Value[0]
+
+#Copy the snapshot to the destination Storage Account
+Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName
---
+++ Start-AzureBackup.ps1
@@ -0,0 +1,2 @@
+﻿Import-Module  'C:\Program Files\Microsoft Azure Recovery Services Agent\bin\Modules\MSOnlineBackup'
+Get-OBPolicy | Start-OBBackup
